59edbdd40e05e0a4d8b78aeeb2f7c926b847606c
Shafi Walsher <shafiwalsher@gmail.com>
2024-01-30 18:18:54 +0530

    video impot fix

--- public/assets/videos/index.ts
+++ public/assets/videos/index.ts
@@ -1,8 +1,5 @@
-// import projectVideo1 from "./twibble.mp4";
-// import projectVideo2 from "./kyoko-anime.mp4";
-// import projectVideo3 from "./i-evnt.mp4";
-const projectVideo1 = require("./twibble.mp4");
-const projectVideo2 = require("./kyoko-anime.mp4");
-const projectVideo3 = require("./i-evnt.mp4");
+import projectVideo1 from "./twibble.mp4";
+import projectVideo2 from "./kyoko-anime.mp4";
+import projectVideo3 from "./i-evnt.mp4";
 
 export { projectVideo1, projectVideo2, projectVideo3 };
